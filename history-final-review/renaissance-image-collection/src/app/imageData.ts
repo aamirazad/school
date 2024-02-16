89237c2b5968639e9674e9f700388055280521d3
Aamir Azad <aamirmazad@gmail.com>
2024-02-16 18:13:30 -0500

    Carousel working!

---
+++ history-final-review/renaissance-image-collection/src/app/imageData.ts
@@ -0,0 +1,15 @@
+import wedding from "./images/wedding.webp";
+
+export const imageData = [
+  {
+    src: wedding,
+    alt: "Wedding image",
+    caption: `<h1>hello</h1>`,
+  },
+  {
+    src: wedding,
+    alt: "Alt text for image 2",
+    caption: "Caption for image 2",
+  },
+  // Add more objects as needed
+];
