9bbd3732979e11e589b7704a8fd1136b10a45e47
Aamir Azad <aamirmazad@gmail.com>
2024-02-24 14:37:47 +0000

    Move everything
    Signed-off-by: Aamir Azad <aamirmazad@gmail.com>

---
+++ src/app/imageData.ts
@@ -0,0 +1,13 @@
+export const imageData = [
+  {
+    src: '/wedding.webp',
+    alt: "Wedding image",
+    caption: `<h1>hello</h1>`,
+  },
+  {
+    src: '/wedding.webp',
+    alt: "Alt text for image 2",
+    caption: "Caption for image 2",
+  },
+  // Add more objects as needed
+];
