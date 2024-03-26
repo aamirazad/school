5de35b692924d1f835d220c9967682f82e07eff2
Aamir Azad <aamirmazad@gmail.com>
2024-03-26 00:42:53 +0000

    Continue writing for the image collection
    Signed-off-by: Aamir Azad <aamirmazad@gmail.com>

--- src/app/renaissance-image-collection/imageData.ts
+++ src/app/renaissance-image-collection/imageData.ts
@@ -4,7 +4,7 @@ export const imageData = [
     alt: "Brunelleschi's dome",
     title: "Florence Cathedral dome by Brunelleschi",
     caption:
-      "This dome was constructed by Filippo Brunelleschi who was a architect and engineer in Italy. The reason why this dome is special is it highlights a lot of new techniques and ideas from the Renaissance. One of these new techniques is Perspective. In order to make the dome look bigger Brunelleschi, made the other buildings around it small. The ability to make a dome during this time was also special because most people were not able to make a dome of that size without supports. He did this with technique's like alternating the bricks and using two domes, one inside and one outside",
+      "This dome was constructed by Filippo Brunelleschi, who was an architect and engineer in Italy. The reason why this dome is special is that it highlights a lot of new techniques and ideas from the Renaissance. One of these new techniques is Perspective. In order to make the dome look bigger, Brunelleschi made the other buildings around it small. The ability to make a dome during this time was also special because most people were not able to make a dome of that size without supports. He did this with techniques like alternating the bricks and using two domes, one inside and one outside",
     cite: [],
   },
   {
@@ -12,7 +12,7 @@ export const imageData = [
     alt: "Mona lisa",
     title: "Mona Lisa Painting by Leonardo da Vinci",
     caption:
-      "This painting is so well known for good reason. It shows many new Renaissance influences such as it's focus on humanism, realism, and perspective. It is humanistic because it is a painting of a person, showing all the qualities of the human. It shows religious aspects which is what pre-humanistic art mostly looked like. It is very realistic showing all the detail's in her face as well as in the background. Perspective is a new technique that sets Leonardo da Vinci apart. It can be seen in the background which creates depth and makes her face pop out.",
+      "This painting is so well known for good reason. It shows many new Renaissance influences, such as its focus on humanism, realism, and perspective. It is humanistic because it is a painting of a person, showing all the qualities of a human. Furthermore, it shows religious aspects, which is what pre-humanistic art mostly looked like. It is very realistic, showing all the details in her face as well as in the background. Perspective is a new technique that sets Leonardo da Vinci apart. It can be seen in the background, which creates depth and makes her face pop out.",
     cite: [
       "https://newportbrushstrokes.com/why-was-the-mona-lisa-important-to-the-renaissance/",
     ],
@@ -22,15 +22,15 @@ export const imageData = [
     alt: "The creation of adam",
     title: "The Creation of Adam by Michelangelo",
     caption:
-      "Is a fresco painting by Michelangelo which goes in to part of the Sistine Chapel's ceiling. The painting represents the a scene in the Book of Genesis where god Gives life to Adam. Renaissance characteristics is very much present with it's use of realism and perspective. Humanism is also shown with it's questioning of the creation of man",
-    cite: [],
+      "This, now extremely well known, painting by Michelangelo is a fresco painting by Michelangelo that goes into part of the Sistine Chapel's ceiling. The painting represents a scene in the Book of Genesis where God gives life to Adam. Renaissance characteristics are very much present in its use of realism and perspective. Shading is used to give a sense of depth behind Adam. God's outstreched hand is almost, but not quite, touching Adam's hand to give him life, who would give life to the rest of mankind. Humanism is also shown in very interesting connection back from God to the human. Some belive the people behind God is a represenation of the human brain's anatomy, a very accurate one in that. In addition the red cloth around god is similar to the human uterus, symbolizing how God created Adam. The painting is surrounded by hundreds of square meters of Biblical art pieces on the roof of the Sistine Chapel",
+    cite: ["https://artincontext.org/the-creation-of-adam-by-michelangelo/"],
   },
   {
     src: "miracle-of-st-mark.webp",
     alt: "Miracle of St. Mark",
     title: "Miracle of St. Mark by Tintoretto",
     caption:
-      "This is one of the most obvious use of color. During this time, there was no artificial dyes so all colors had to be made from natural sources. This means some colors were expensive and sometimes almost impossible to get",
+      "This is one of the most obvious use of color. During this time, there was no artificial dyes so all colors had to be made from natural sources. This means some colors were expensive and sometimes almost impossible to get. Jacopo Tintoretto uses these colors to represent life, showing St Mark to resue a slave. The art peaice has the feeling of a play, with all the action and the crowd watching",
     cite: [
       "https://www.gallerieaccademia.it/en/miracle-slave-also-known-miracle-saint-mark",
     ],
