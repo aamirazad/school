ef3953208caa9a76ef42b3b24dcbd39bcf161775
Aamir Azad <aamirmazad@gmail.com>
2024-10-18 17:19:58 -0400

    Project updates

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,6 @@ const nextConfig = {
         protocol: "https",
         hostname: "cdn.aamira.me",
         port: "",
-        pathname: "/api/cdn/**",
-      },
-      {
-        protocol: "http",
-        hostname: "192.168.87.228",
-        port: "50003",
-        pathname: "/api/cdn/**",
       },
     ],
   },
