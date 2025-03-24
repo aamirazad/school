f929c3588b476d793ebb26e662010eed9440f4ff
Aamir Azad <aamirmazad@gmail.com>
2025-03-24 18:31:22 -0400

    removed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,12 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-import { Geist } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "School projects home",
   description: "A collection of the mini projects I've worked on for school",
 };
-
-const geist = Geist({
-  subsets: ["latin"],
-});
 
 export default function RootLayout({
   children,
@@ -21,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geist.className} antialiased text-slate-100 bg-black`}
-      >
+      <body className={` antialiased text-slate-100 bg-black`}>
         <div className="absolute top-0 left-0 mt-1 ml-2">
           <Link href="/">Back</Link>
         </div>
