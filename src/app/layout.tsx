84baf98c32583498176cc8e59cb5c973b011c40b
Aamir Azad <aamirmazad@gmail.com>
2025-03-24 18:59:22 -0400

    add back geist

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 import "./globals.css";
+import { Geist } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "School projects home",
   description: "A collection of the mini projects I've worked on for school",
 };
+
+const geist = Geist({
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -15,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={` antialiased text-slate-100 bg-black`}>
+      <body
+        className={` antialiased text-slate-100 bg-black ${geist.className}`}
+      >
         <div className="absolute top-0 left-0 mt-1 ml-2">
           <Link href="/">Back</Link>
         </div>
