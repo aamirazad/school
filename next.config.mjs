ff51e1484458f6531f711301eb442bf58d8c9a12
Aamir Azad <aamirmazad@gmail.com>
2025-11-30 21:44:16 -0500

    fix: packages were broken from the update

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "xrgkyc5aexvkc7oh.public.blob.vercel-storage.com",
-        port: "",
-      },
-    ],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "xrgkyc5aexvkc7oh.public.blob.vercel-storage.com",
+				port: "",
+			},
+		],
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
 };
 
 export default nextConfig;
